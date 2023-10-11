f26ca8a87b7203c5a72fa459277fd5273697af94
ArcZLeo <79063417+ArcZLeo@users.noreply.github.com>
2023-10-11 12:13:47 -0500

    Create Api.py

---
+++ codigo api/Api.py
@@ -0,0 +1,17 @@
+#se creo un simple api la cual recibira las coordenadas del punto del mapa que necesitamos para poder realizar el metodo de los poligonos
+from flask import Flask, jsonify, request,send_file
+
+app = Flask()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
+
+@app.route('/poligonosT', methods = ['GET'])
+def hello():
+    x = request.args.get('x')
+    y = request.args.get('y')
+  
+    cordenada = x + y
+
+    return text
